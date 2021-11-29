8eec79adcafd2818c3eee91e3760d73c5fb01f96
xuewenkang <2601624788@qq.com>
2021-11-29 22:07:06 +0800

    添加Actor代码

---
+++ sharedata/src/main/java/org/example/Client.java
@@ -0,0 +1,12 @@
+package org.example;
+
+/**
+ * @Auther: kangkang
+ * @Date: 2021/11/18 10:44
+ * @Description:
+ */
+public class Client {
+    public <SyncRpc> Client(ShareKey shareKey, SyncRpc syncRpc) {
+
+    }
+}
